c42ec86ed1684f081204a3aee5489a9f59661cf6
Anna Manukian <amanu001@odu.edu>
2019-04-05 17:25:28 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,10 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(n)
-  n = n.to_i
-  n = n-1
+  n = n.to_i - 1
 end
+
 
 def move(array, index, token="X")
   array[index] = token
